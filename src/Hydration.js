922499771142fe495a898a5f59f2ff6182294dbe
Veronica Andrade <veronica726@gmail.com>
2020-01-02 14:42:36 -0700

    Refactor drink method a bit to be more readable

--- src/Hydration.js
+++ src/Hydration.js
@@ -6,10 +6,8 @@ class Hydration {
     this.drink(userRepository);
   }
   drink(userRepo) {
-    var hydrate = this;
-    userRepo.users.find(function(user) {
-      return user.id === hydrate.userId;
-    }).updateHydration(this.date, this.ounces);
+    let mainUser = userRepo.users.find(user => user.id === this.userId)
+    return mainUser.updateHydration(this.date, this.ounces);
   }
 }
 
